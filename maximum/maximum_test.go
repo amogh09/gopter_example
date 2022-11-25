412291af38a4eafe315d895944481032d653360b
Amogh Rathore <amoghdroid09@gmail.com>
2022-11-25 15:30:24 -0800

    Refactoring

--- maximum/maximum_test.go
+++ maximum/maximum_test.go
@@ -12,27 +12,25 @@ import (
 func TestMaximum(t *testing.T) {
 	properties := gopter.NewProperties(&gopter.TestParameters{MinSize: 1})
 
-	properties.Property("maximum is greater than or equal to all",
-		prop.ForAll(
-			func(nums []int) bool {
-				m := Maximum(nums)
-				for _, n := range nums {
-					if m < n {
-						return false
-					}
+	properties.Property("maximum is greater than or equal to all", prop.ForAll(
+		func(nums []int) bool {
+			m := Maximum(nums)
+			for _, n := range nums {
+				if m < n {
+					return false
 				}
-				return true
-			},
-			gen.SliceOf(gen.Int()),
-		))
+			}
+			return true
+		},
+		gen.SliceOf(gen.Int()),
+	))
 
-	properties.Property("maximum must be present in the slice",
-		prop.ForAll(
-			func(nums []int) bool {
-				return slices.Contains(nums, Maximum(nums))
-			},
-			gen.SliceOf(gen.Int()),
-		))
+	properties.Property("maximum must be present in the slice", prop.ForAll(
+		func(nums []int) bool {
+			return slices.Contains(nums, Maximum(nums))
+		},
+		gen.SliceOf(gen.Int()),
+	))
 
 	properties.TestingRun(t)
 }
